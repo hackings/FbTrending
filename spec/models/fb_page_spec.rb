93b514cda228bc4dc339334c3614ba194d9df4c2
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:23:24 +0530

    validation specs for fb_page model

--- spec/models/fb_page_spec.rb
+++ spec/models/fb_page_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe FbPage do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it{
+    should validate_presence_of(:business_name)
+    should validate_presence_of(:facebook_url)
+    should validate_presence_of(:email)
+
+    should allow_value("test@example.com").for(:email)
+    should_not allow_value("no_email").for(:email)
+  }
+
+
 end
