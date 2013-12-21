90ab62849abb258f839d81d8a26da335e6ad52b6
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:28:16 +0530

    scope spec#to get top 30 order by likes

--- spec/models/fb_page_spec.rb
+++ spec/models/fb_page_spec.rb
@@ -9,6 +9,18 @@ describe FbPage do
     should allow_value("test@example.com").for(:email)
     should_not allow_value("no_email").for(:email)
   }
+  
+ it "should return top 30 pages order by fb likes" do
+   page1 = Fabricate(:fb_page) 
+   page2 = Fabricate(:fb_page)
 
+   page1.update_column( :facebook_likes, 10)
+   page2.update_column( :facebook_likes, 20)
+
+   top_results = [ page2, page1 ]
+
+   expect( FbPage.top_30_trands.all ).to eq(top_results)
+
+ end
 
 end
