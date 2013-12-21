ddf7233b288ba03423dbf2b5d3d136827a1fb6e9
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:29:58 +0530

    fix failure model spec to get top 30 order by likes

--- app/models/fb_page.rb
+++ app/models/fb_page.rb
@@ -3,4 +3,5 @@ class FbPage < ActiveRecord::Base
  validates :business_name, :facebook_url, :email, presence: true
  validates :email, format: { with: /\A(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})\Z/, allow_blank: true }
 
+ scope :top_30_trands, order("facebook_likes DESC").limit(30)
 end
