4f932e69bdca62ca5b31282fe49efdc203756a83
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 19:18:01 +0530

    fix controller create specs

--- app/controllers/fb_pages_controller.rb
+++ app/controllers/fb_pages_controller.rb
@@ -8,5 +8,19 @@ class FbPagesController < ApplicationController
   end
 
   def create
+    @fb_page = FbPage.new(fb_page_params)
+    respond_to do |format|
+      if @fb_page.save
+        format.html { redirect_to fb_pages_path, notice: 'Fb page was successfully created.' }
+      else  
+        format.html {  render action: 'new' }
+      end
+    end 
+  end
+
+private
+  
+  def fb_page_params
+    params.require(:fb_page).permit(:business_name, :facebook_url, :description, :name, :email, :facebook_likes)
   end
 end
