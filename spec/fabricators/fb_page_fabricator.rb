a32c4ac8ebbe029ba277619de11ccd20d82fb4c2
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 18:00:36 +0530

    rails generate fabrication:model FbPage to fix

---
+++ spec/fabricators/fb_page_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:fb_page) do
+  business_name  "MyString"
+  facebook_url   "MyString"
+  description    "MyText"
+  name           "MyString"
+  email          "MyString"
+  facebook_likes 1
+end
