0ae2bef43c4e76ba75c6058fa0744f1a16db2c93
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 19:27:57 +0530

    model validation to fix create with invalid attributes

--- app/models/fb_page.rb
+++ app/models/fb_page.rb
@@ -1,2 +1,6 @@
 class FbPage < ActiveRecord::Base
+
+ validates :business_name, :facebook_url, :email, presence: true
+ validates :email, format: { with: /\A(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})\Z/, allow_blank: true }
+
 end
