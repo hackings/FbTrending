15977fae32035c50eb41ac36e654acb15fbdd601
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:16:52 +0530

    view spec for new template

--- spec/views/fb_pages/new.html.erb_spec.rb
+++ spec/views/fb_pages/new.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe "fb_pages/new.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders new form with inputs" do 
+   assign(:fb_page, Fabricate.build(:fb_page))
+   render
+
+   assert_select "#fb_page_business_name", :name => "fb_page[business_name]"
+   assert_select "#fb_page_description", :name => "fb_page[description]" 
+   assert_select "#fb_page_name", :name => "fb_page[name]" 
+   assert_select "#fb_page_email", :name => "fb_page[email]" 
+  end
 end
