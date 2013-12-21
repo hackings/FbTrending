bb5dadc885fd6b9ca399fdad737c0ec1f664dc10
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:44:29 +0530

    get fb likes of the page while create the entry

--- app/models/fb_page.rb
+++ app/models/fb_page.rb
@@ -4,6 +4,8 @@ class FbPage < ActiveRecord::Base
  validates :business_name, :facebook_url, :email, presence: true
  validates :email, format: { with: /\A(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3})\Z/, allow_blank: true }
 
+ before_create :get_likes
+
  scope :top_30_trands, order("facebook_likes DESC").limit(30)
 
  def self.update_likes
