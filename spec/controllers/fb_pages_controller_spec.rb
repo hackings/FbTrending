150180b096d6c9980d9b5d0d3a94a2a482b1e85e
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 17:34:24 +0530

    rspec:controller generate only  actions

---
+++ spec/controllers/fb_pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe FbPagesController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "POST 'create'" do
+    it "returns http success" do
+      post 'create'
+      response.should be_success
+    end
+  end
+
+end
