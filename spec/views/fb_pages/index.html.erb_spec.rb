3a2a95b776cf8347a71bc4a30a7ba0a90c250d2c
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 20:06:15 +0530

    assign @fb_pages variable to list the page content

--- spec/views/fb_pages/index.html.erb_spec.rb
+++ spec/views/fb_pages/index.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "fb_pages/index.html.erb" do
   it "list all the pages" do
+    assign(:fb_pages, [ Fabricate(:fb_page) ])
     render
     expect(rendered).to match /Listing Facebook Trands/
     expect(rendered).to match /Business name/ 
