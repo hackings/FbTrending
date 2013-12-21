064c83bf63ec4bb9f6e278cf9c8659058ae6028a
Vishnu Atrai <me@vishnuatrai.com>
2013-12-21 17:14:10 +0530

    blank skleton of new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbTrending::Application.config.secret_key_base = 'd61a4544bf156f90905335ce7ad66e201ae466bb8f9bec4cf6a92617b88af1faf3b543a03758c29425d27972d11f1116b1ffb321db3226498819c86fba11c7b1'
